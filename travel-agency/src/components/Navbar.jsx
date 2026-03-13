a983f78ad5a12f6ea452da3bec7759887949690a
Your Name <you@example.com>
2026-03-13 14:23:34 +0100

    imaggini

--- travel-agency/src/components/Navbar.jsx
+++ travel-agency/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="bg-white shadow-md p-4 flex justify-between">
+    <nav className="bg-gray-900  text-white shadow-md p-4 flex justify-between">
       <h1 className="font-bold text-xl text-blue-600">
         TravelDream
       </h1>
